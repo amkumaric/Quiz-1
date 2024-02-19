454fadd8396cf6b97367c6b9c40c706744c8891b
Anthony <anthony_kumaric1@baylor.edu>
2024-02-19 15:45:24 -0600

    Final

--- processStudents.py
+++ processStudents.py
@@ -32,49 +32,53 @@ import csv
 
 
 # create a file object to open the file in read mode
-
+infile = open("students.csv", "r")
 
 
 # create a csv object from the file object
-
+csv_file = csv.reader(infile)
 
 #skip the header row
-
+next(csv_file)
 
 #create an outfile object for the pocessed record
-
-
+outfile = open("processedStudents.csv", "a", newline="")
 
 #create a new dictionary named 'student_dict'
-
+student_dict = {}
 
 
 #use a loop to iterate through each row of the file
-
+for row in csv_file:
 
     #check if the GPA is below 3.0. If so, write the record to the outfile
-    
-        
-
+    first_name = row[2]
+    last_name = row[3]
+    full_name = (f'{first_name.title()} {last_name.title()}')
+    gpa = float(row[8])
+    stud_id = row[0]
+    major = row[6]
+    classification = [7]
+    if gpa < 3.00: 
+        outfile.write(f'{stud_id},{first_name},{last_name},{major},{classification}{str(gpa)}')
 
 
     # append the record to the dictionary with the student Full name in proper case 
     # as the Key and the value as the GPA
+        student_dict[full_name] = gpa
     
 
 
-
-
-
 #print the entire dictionary
+print(student_dict)
 
 
 #Print the corresponding GPA for student 'Luke Brazzi'
-
+print(student_dict["Luke Brazzi"])
 
 
 #close the outfile
-
+outfile.close()
 
 
 
